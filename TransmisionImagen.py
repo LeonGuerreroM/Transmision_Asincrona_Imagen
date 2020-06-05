de341d240067102d41514c15c175db55b6d50c97
LeonGuerreroM <leonmora79@gmail.com>
2020-06-05 00:14:31 -0500

    version de prueba completa

--- TransmisionImagen.py
+++ TransmisionImagen.py
@@ -48,3 +48,112 @@ for i in binarios:
     indiceBinarios += 1
 
 print(binarios[100])
+
+print("_______________EMISOR________________")
+print("Elija el numero de bits de parada (1 o 2)")
+print("--------------------------------------")
+bits_parada = input()
+bits_parada = int(bits_parada)
+if bits_parada == 1:
+    terminacion = "1"
+    l_esperado = 10
+else:
+    terminacion = "11"
+    l_esperado = 11
+print("--------------------------------------")
+print("Escoja el tipo de paridad\n0. Sin paridad\n1. Paridad impar\n2.Paridad par")
+print("--------------------------------------")
+paridad = input()
+paridad = int(paridad)
+if paridad == 0:
+    l_esperado -= 1
+    corte -= 1
+print("_____________________________________")
+
+if paridad == 0:
+    for i in binarios:
+        mensaje_a_enviar.append(i.zfill(9)+terminacion)
+
+if paridad == 1:
+    for i in binarios:
+        mensaje.append(i.zfill(9))
+    for i in mensaje:
+        mensajeC = str(i)
+        num_unos = mensajeC.count("1")
+        if num_unos%2 == 0:
+            mensajeC=mensajeC+"1"+terminacion
+            mensaje_a_enviar.append(mensajeC)
+        if num_unos%2 != 0: 
+            mensajeC=mensajeC+"0"+terminacion
+            mensaje_a_enviar.append(mensajeC)
+
+if paridad == 2:
+    for i in binarios:
+        mensaje.append(i.zfill(9))
+    for i in mensaje:
+        mensajeC = str(i)
+        num_unos = mensajeC.count("1")
+        if num_unos%2 == 0:
+            mensajeC=mensajeC+"0"+terminacion
+            mensaje_a_enviar.append(mensajeC)
+        if num_unos%2 != 0: 
+            mensajeC=mensajeC+"1"+terminacion
+            mensaje_a_enviar.append(mensajeC)        
+
+print("_______________RECEPTOR________________")
+if paridad == 1:
+    paridadT = "impar"
+elif paridad == 2:
+    paridadT = "par"
+elif paridad == 0:
+    paridadT = "nula"
+print("Se ha escogido paridad {} con {} bit(s) de parada".format(paridadT, bits_parada))
+print("Comprobando bits de inicio")
+indice = 0
+for i in mensaje_a_enviar:
+    mensaje_a_enviar[indice] = i[1:]
+    indice = indice + 1
+time.sleep(2)
+print("Comprobando bits de parada")
+indice = 0
+for i in mensaje_a_enviar:
+    iT = i.count("1") + i.count("0")
+    if iT == l_esperado and i[corte:] == terminacion:
+        mensaje_a_enviar[indice] = i[0:corte]
+    else:
+        mal_transmitidos.append(indice)
+    indice += 1
+time.sleep(2)
+print("Buscando errores con paridad",paridadT)
+indice = 0
+if paridad == 1:
+    for i in mensaje_a_enviar:
+        if i.count("1")%2 == 0:
+            mal_transmitidos.append(indice)
+        if i.count("1")%2 != 0:
+            mensaje_a_enviar[indice] = i[:-1]
+        indice += 1
+if paridad == 2:
+    for i in mensaje_a_enviar:
+        if i.count("1")%2 != 0:
+            mal_transmitidos.append(indice)
+        if i.count("1")%2 == 0:
+            mensaje_a_enviar[indice] = i[:-1]
+        indice += 1
+time.sleep(2)         
+if len(mal_transmitidos) != 0:
+    print("Es necesario retransmitir los bytes: ")
+    print(mal_transmitidos)
+else: 
+    indice = 0
+    for i in mensaje_a_enviar:
+        if int(mensaje_a_enviar[indice], base=2) != int(binPuros[indice], base=2):
+            discrepancia += 1
+            break
+        indice += 1
+    if discrepancia == 0:
+        print("Mostrando imagen")
+        img.show()
+        print("Transmision terminada")
+    else:
+        print("La imagen no fue bien recibida")
